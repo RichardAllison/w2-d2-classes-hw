fc2a9c06f177acf8214ddf504e4ca36328585675
Richard Allison <richard.allison.18@outlook.com>
2017-11-28 22:06:35 +0000

    added test to bear_spec.rb to test Bear eat method

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -9,6 +9,9 @@ class TestBear < MiniTest::Test
   def setup
     @bear1 = Bear.new("Yogi")
     @river = River.new("Volga")
+    @fish1 = Fish.new("Trout")
+    @fish2 = Fish.new("Salmon")
+    @fish3 = Fish.new("Goldfish")
   end
 
   def test_bear_has_name
@@ -17,6 +20,15 @@ class TestBear < MiniTest::Test
 
   def test_stomach_empty
     assert_equal([], @bear1.stomach)
+  end
+
+  def test_eat
+    @river.add_fish(@fish1)
+    @river.add_fish(@fish2)
+    @bear1.eat(@river)
+    @river.remove_fish()
+    assert_equal(1, @bear1.stomach.count)
+    
   end
 
 end
