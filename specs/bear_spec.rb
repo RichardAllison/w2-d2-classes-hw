0dadfc210425e2ea9473cd60cac79510d48bcf1b
Richard Allison <richard.allison.18@outlook.com>
2017-11-28 21:33:27 +0000

    created Bear class, passed first test

---
+++ specs/bear_spec.rb
@@ -0,0 +1,18 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bear')
+require_relative('../fish')
+require_relative('../river')
+
+class TestBear < MiniTest::Test
+
+  def setup
+    @bear1 = Bear.new("Yogi")
+    @river = River.new("Volga")
+  end
+
+  def test_bear_has_name
+    assert_equal("Yogi", @bear1.name)
+  end
+
+end
